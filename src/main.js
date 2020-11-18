6aa142237eac4d4e0726ea7f8056b077c437e54a
DESKTOP-K0BIRC6\Administrator <1452429671@qq.com>
2020-11-18 17:39:38 +0800

    feat: 配置vantui组件自动按需引入

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
-import Vant from 'vant';
-import 'vant/lib/index.css';
+import { Button } from 'vant';
+// import 'vant/lib/index.css';
 
 createApp(App)
-    .use(Vant)
+    .use(Button)
     .use(store)
     .use(router)
     .mount('#app');
