7d6fc4b99d733d07bd5bdb651ad885dc4440228f
DESKTOP-K0BIRC6\Administrator <1452429671@qq.com>
2020-11-18 17:32:22 +0800

    feat：完成初始化、配置代码风格、vw适配、集成want

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import Vant from 'vant';
+import 'vant/lib/index.css';
 
 createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+    .use(Vant)
+    .use(store)
+    .use(router)
+    .mount('#app');
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home"></div>
+    <van-button type="primary">URL 跳转</van-button>
+    <button class="button">button</button>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
+    name: 'Home',
+    components: {},
 };
 </script>
+
+<style lang="scss" scoped>
+.button {
+    width: 702px;
+    height: 98px;
+    margin: 0 auto;
+    display: block;
+}
+</style>
